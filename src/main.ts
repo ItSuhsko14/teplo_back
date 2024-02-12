f5a682cb3322d70ca0827837685b1ab25913ee91
ItSushko14 <itsushkoandriy@gmail.com>
2024-02-12 17:37:58 +0200

    change link for cors policy

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
   });
 
   app.enableCors({
-    origin: ["*", "http://localhost:3000", "https://subtle-rabanadas-560bb7.netlify.app/"],
+    origin: ["*", "http://localhost:3000", "https://subtle-rabanadas-560bb7.netlify.app/admin/"],
     methods: "GET,PUT,POST,DELETE",
     allowedHeaders: "Content-Type, Authorization",
   });
